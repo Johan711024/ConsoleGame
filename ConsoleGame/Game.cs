d225b6327f4d58d394cd29fb076a7e0be3ae6ce4
Johan Svensson <hyperjohan@hotmail.com>
2022-11-13 20:32:22 +0100

    Add project files.

---
+++ ConsoleGame/Game.cs
@@ -0,0 +1,43 @@
+﻿internal class Game
+{
+
+
+    public Game()
+    {
+        
+    }
+
+    internal void Run()
+    {
+        Initialize();
+        Play();
+
+    }
+
+    private void Play()
+    {
+        bool gameInProgress = true;
+
+        do
+        {
+            //DrawMap
+            //GetCommand
+            GetCommand();
+            //Act
+            //DrawMap
+            //EnemyAction
+            //DrawMap
+
+        } while (gameInProgress);
+    }
+
+    private void GetCommand()
+    {
+        var KeyPressed = ConsoleUI
+    }
+
+    private void Initialize()
+    {
+        
+    }
+}
